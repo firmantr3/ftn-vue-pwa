e0cf41912004385f0923025f935bd5d7fdf7c0cc
Firman Taruna Nugraha <firmantr3@gmail.com>
2019-01-11 10:19:07 +0700

    update dependencies, upstream api changes

--- vue.config.example.js
+++ vue.config.example.js
@@ -1,7 +1,7 @@
 const webpack = require('webpack')
 
 module.exports = {
-    baseUrl: process.env.NODE_ENV === 'production'
+    publicPath: process.env.NODE_ENV === 'production'
         ? '/'
         : '/',
     configureWebpack: {
