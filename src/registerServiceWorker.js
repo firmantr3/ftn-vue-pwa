459ed4af9fb13964c77a27d73b61e7ae4a787827
Firman Taruna Nugraha <firmantr3@gmail.com>
2019-02-18 03:17:11 +0700

    custom service worker updater

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -1,6 +1,24 @@
 /* eslint-disable no-console */
 
 import { register } from 'register-service-worker'
+import swal from 'sweetalert2'
+import i18n from '@/plugins/i18n'
+
+const notifyUserAboutUpdate = worker => {
+  swal({
+    type: 'question',
+    title: i18n.t('update_available'),
+    text: i18n.t('update_now'),
+    reverseButtons: true,
+    showCancelButton: true,
+    confirmButtonText: i18n.t('ok'),
+    cancelButtonText: i18n.t('cancel')
+  }).then((result) => {
+    if(result.value) {
+      worker.postMessage({ action: 'skipWaiting' })
+    }
+  })
+}
 
 if (process.env.NODE_ENV === 'production') {
   register(`${process.env.BASE_URL}service-worker.js`, {
@@ -13,8 +31,9 @@ if (process.env.NODE_ENV === 'production') {
     cached () {
       console.log('Content has been cached for offline use.')
     },
-    updated () {
+    updated (registration) {
       console.log('New content is available; please refresh.')
+      notifyUserAboutUpdate(registration.waiting)
     },
     offline () {
       console.log('No internet connection found. App is running in offline mode.')
@@ -24,3 +43,10 @@ if (process.env.NODE_ENV === 'production') {
     }
   })
 }
+
+var refreshing
+navigator.serviceWorker.addEventListener('controllerchange', function () {
+  if (refreshing) return
+  window.location.reload()
+  refreshing = true
+})
---
+++ src/service-worker.js
@@ -0,0 +1,7 @@
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+self.addEventListener("message", (e) => {
+    if (e.data.action == 'skipWaiting') self.skipWaiting()
+})
