9f33af13bd299fb23a9388cf361bd22453bcb62f
Firman Taruna Nugraha <firmantr3@gmail.com>
2020-02-06 22:23:21 +0700

    fixes the sw workbox error

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,5 +1,4 @@
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 self.addEventListener("message", (e) => {
